e08667729cdccf16e41c22538310e470feba37a8
Almudena Rendón Fernández <126793941+Almudena-Rendon@users.noreply.github.com>
2025-03-22 22:15:53 +0100

    style: Add almucodes logo in project

--- src/utils/icons/index.js
+++ src/utils/icons/index.js
@@ -1,14 +1,7 @@
+import Verano from '../icons/svg/verano.svg'
+import Morty from '../icons/svg/morty.svg'
+import Jerry from '../icons/svg/jerry.svg'
+import Beth from '../icons/svg/beth.svg'
+import Rick from '../icons/svg/rick.svg'
 
-import Verano from "../icons/svg/verano.svg"
-import Morty from "../icons/svg/morty.svg"
-import Jerry from "../icons/svg/jerry.svg"
-import Beth from "../icons/svg/beth.svg"
-import Rick from "../icons/svg/rick.svg"
-
-export { 
-    Morty,
-    Verano,
-    Jerry,
-    Beth,
-    Rick
-}
+export { Morty, Verano, Jerry, Beth, Rick }
